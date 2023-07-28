c0b8cb2078d446f4f92db2f82572ef25b9e884db
FireGroup <FireGroup@huypg.firegroup.local>
2023-07-28 16:51:50 +0700

    Update TS_Collection_Register

--- Test Suites/TS_Collection_Register.ts
+++ Test Suites/TS_Collection_Register.ts
@@ -4,8 +4,8 @@
    <name>TS_Collection_Register</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
-   <executionMode>SEQUENTIAL</executionMode>
-   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <executionMode>PARALLEL</executionMode>
+   <maxConcurrentInstances>1</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
          <configuration>
