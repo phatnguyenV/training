588484f127589e77843904c1b0d449fb9e900d79
FireGroup <FireGroup@huypg.firegroup.local>
2023-07-28 16:18:03 +0700

    Update TS_Register

--- Test Suites/TS_Register.ts
+++ Test Suites/TS_Register.ts
@@ -54,4 +54,47 @@
          <variableId>cab7afc9-32b2-44ed-a927-c38680d3ad80</variableId>
       </variableLink>
    </testCaseLink>
+   <testCaseLink>
+      <guid>fb96db99-7c05-4f82-8bf6-75f925ff83e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_Register</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>68a0609c-5cf2-463e-8a35-79aa2615b365</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d0940fb7-e0e3-4be3-812f-a537ee6b3313</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>815611cb-ff89-4dbe-8797-86e93857ccdc</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>db443020-32ac-47da-8221-eb4da1ba49b7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f9a12aa7-5635-45de-9f4f-b10f48ecacb9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>cab7afc9-32b2-44ed-a927-c38680d3ad80</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
